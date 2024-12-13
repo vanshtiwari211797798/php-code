544f5696d3826b9ee3f5ecdbd1fc9ef8ada0464c
vanshtiwari211797798 <vanshtiwari9091@gmail.com>
2024-12-13 11:03:44 +0530

    array functions

--- ArrayFunctions.php
+++ ArrayFunctions.php
@@ -30,9 +30,27 @@
             // echo "<pre>";
             // print_r(array_merge($ar, $ar2));
 
-            $a  = "5";
-            $b = "21";
-            $c = 'vansh';
-            print_r(compact('a','b','c'));
-    
+            // convert vriable to array
+            // $a  = "5";
+            // $b = "21";
+            // $c = 'vansh';
+            // print_r(compact('a','b','c'));
+
+
+            // sorting array into assending order
+                // $ar = [1,3,8,9,5,6,5];
+                // sort($ar);
+                // print_r($ar);
+
+                // sorting array into desending order
+                // $ar = [1,6,9,5];
+                // rsort($ar);
+                // print_r($ar);
+
+                // combine the array -> two array is required - > first array is key and second array is value type
+                $key = range('a', 'f');
+                $value = range(0,5);
+                print_r(array_combine($key, $value));
+
+
 ?>  
