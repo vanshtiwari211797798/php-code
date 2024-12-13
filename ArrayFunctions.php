5f3065b946acd4b30475e6c76341f9d7f2607b75
vanshtiwari211797798 <vanshtiwari9091@gmail.com>
2024-12-13 10:32:33 +0530

    changes

--- ArrayFunctions.php
+++ ArrayFunctions.php
@@ -18,9 +18,21 @@
     //    print_r($arr);
 
                // array_unshift -> remove element of the first index
-               $arr = [1,2,3,54,6,5,6,"vansh"];
-               array_shift($arr);
-               echo "<pre>";
-               print_r($arr);
+            //    $arr = [1,2,3,54,6,5,6,"vansh"];
+            //    array_shift($arr);
+            //    echo "<pre>";
+            //    print_r($arr);
+
+
+            // Merge two array using using merge method of array
+            // $ar = [1,2,3,4];
+            // $ar2 = [5,6,7,8];
+            // echo "<pre>";
+            // print_r(array_merge($ar, $ar2));
+
+            $a  = "5";
+            $b = "21";
+            $c = 'vansh';
+            print_r(compact('a','b','c'));
     
 ?>  
