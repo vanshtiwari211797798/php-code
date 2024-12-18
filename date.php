efda9e82a3a0b7cdc94a9215c72d66ee8de34576
vanshtiwari211797798 <vanshtiwari9091@gmail.com>
2024-12-18 12:24:05 +0530

    includemethod

--- date.php
+++ date.php
@@ -11,7 +11,17 @@
    echo "<br>";
    echo date_default_timezone_get();
    echo "<br>";
-   echo 'current hours is'. "&nbsp;". Date('H-i-s-a'); // current hours minutes and second -> i for minutes, h for hours and s for seconds
+   echo 'current hours is'. "&nbsp;". Date('H-i-s-a') ."<br/>"; // current hours minutes and second -> i for minutes, h for hours and s for seconds
+   echo  'current date and time is ' .date("d/m/Y h:i:s:a") ."<br/>"; // current year, month, date, hours, minutes and seconds
+   echo date("05-05-2020-a");// -> mktime is also used liked this;
+   $a = mktime(11, 05, 41, 12, 17, 2025);
+   echo "<br/>";
+   echo date("Y-m-d h:i:s:a",$a) ."<br>";
 
+
+//    String to time conversion using strtotime methods
+
+    $b= strtotime("10:20a 12 june");
+    echo date("Y-m-d h:i:s:a",$b) ."<br>";
    
 ?>
---
+++ includeMethod.php
@@ -0,0 +1,4 @@
+<?php
+    // include "file_name.extension"; // used for include other file in the code like header.php or footer.php;
+    // require "file_name.php"; //it is alse used like include method -> if file not exist then error show and code not be run any code
+?>
